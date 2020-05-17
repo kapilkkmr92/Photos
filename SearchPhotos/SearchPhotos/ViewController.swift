f30d3d8388a3694fde6700a0962bb71ecf407c23
kapil <kapil.kumar@github.com>
2020-05-17 17:36:07 +0530

    created project

---
+++ SearchPhotos/SearchPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchPhotos
+//
+//  Created by Kapil.kumar on 17/05/20.
+//  Copyright © 2020 Kapil.kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SearchPhotos/SearchPhotosTests/SearchPhotosTests.swift
@@ -0,0 +1,34 @@
+//
+//  SearchPhotosTests.swift
+//  SearchPhotosTests
+//
+//  Created by Kapil.kumar on 17/05/20.
+//  Copyright © 2020 Kapil.kumar. All rights reserved.
+//
+
+import XCTest
+@testable import SearchPhotos
+
+class SearchPhotosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
